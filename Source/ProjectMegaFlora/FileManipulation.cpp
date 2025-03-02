6ff37b5fe13bffdde385f603dd3fa45fc281c140
greatbrick <speharley@gmail.com>
2025-03-02 01:22:01 -0500

    Added Back Deleting Save Slot Thumbnails

--- Source/ProjectMegaFlora/FileManipulation.cpp
+++ Source/ProjectMegaFlora/FileManipulation.cpp
@@ -1,5 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "FileManipulation.h"
 
+void UFileManipulation::DeleteFile(const FString& FilePath)
+{
+    if (!FilePath.IsEmpty())
+    {
+        if (FPaths::ValidatePath(FilePath) && FPaths::FileExists(FilePath))
+        {
+            IFileManager& FileManager = IFileManager::Get();
+            FileManager.Delete(*FilePath);
+        }
+    }
+}
--- Source/ProjectMegaFlora/FileManipulation.h
+++ Source/ProjectMegaFlora/FileManipulation.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,11 +5,13 @@
 #include "FileManipulation.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class PROJECTMEGAFLORA_API UFileManipulation : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+public:
+	UFUNCTION(BlueprintCallable, Category = "FileManagement")
+	static void DeleteFile(const FString& FilePath);
 };
